c5c4469da625159f1086f9f56fd26926e18c81b3
Alexey <44631357+agmitron@users.noreply.github.com>
2023-03-14 23:19:06 +0800

    prepare: 14.03.2023 #2

---
+++ 14.03.2023/webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  devServer: {
+    port: 9000
+  }
+};
