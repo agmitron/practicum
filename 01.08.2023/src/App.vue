b274cd9df33bfecd2233718f6ac6b2e5c59e6b05
Aleksei Gmitron <me@agmitron.dev>
2023-08-01 20:53:35 +0700

    prepare: 01.08.2023

---
+++ 01.08.2023/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <Dropdown
+      v-on:decrement="(what) => decrement('1', what)"
+      v-on:increment="(what) => increment('1', what)"
+      :options="dropdowns[1]"
+    />
+
+    <Dropdown
+      v-on:decrement="(what) => decrement('2', what)"
+      v-on:increment="(what) => increment('2', what)"
+      :options="dropdowns[2]"
+    />
+
+    <Dropdown
+      v-on:decrement="(what) => decrement('3', what)"
+      v-on:increment="(what) => increment('3', what)"
+      :options="dropdowns[3]"
+    />
+  </div>
+</template>
+
+<script>
+import Dropdown from "./components/Dropdown.vue";
+
+export default {
+  name: "App",
+  components: {
+    Dropdown,
+  },
+  data() {
+    return {
+      dropdowns: {
+        1: {
+          спальни: 1,
+          кровати: 2,
+        },
+        2: {
+          кухни: 1,
+          санузлы: 2,
+        },
+        3: {
+          кофемашины: 10,
+          "стиральные машины": 0,
+        },
+      },
+    };
+  },
+  methods: {
+    increment(dropdown, what) {
+      this.dropdowns[dropdown][what] = this.dropdowns[dropdown][what] + 1;
+    },
+    decrement(dropdown, what) {
+      this.dropdowns[dropdown][what] = this.dropdowns[dropdown][what] - 1;
+    },
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap");
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Montserrat", sans-serif;
+}
+</style>
---
+++ 01.08.2023/src/components/Dropdown.vue
@@ -0,0 +1,127 @@
+<template>
+  <label
+    class="dropdown dropdown_first"
+    v-bind:class="{ dropdown_open: isOpen }"
+  >
+    <div class="dropdown__value">
+      <span class="dropdown__value-text">Пусто</span>
+      <button class="dropdown__expand-button" v-on:click="toggle"></button>
+    </div>
+    <ul class="dropdown__options">
+      <li class="dropdown__option" v-for="([name, count]) in Object.entries(options)" :key="name">
+        <span class="dropdown__option-text">{{ name }}</span>
+        <button class="dropdown__option-control" @click="() => decrement(name)">-</button>
+        <span class="dropdown__option-count">{{ count }}</span>
+        <button class="dropdown__option-control" @click="() => increment(name)">+</button>
+      </li>
+    </ul>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "MyDropdown",
+  props: {
+    options: Object,
+  },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    },
+    increment(what) {
+      this.$emit('increment', what)
+    },
+    decrement(what) {
+      this.$emit('decrement', what)
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dropdown {
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+  max-width: 266px;
+  width: 100%;
+}
+
+.dropdown__value,
+.dropdown__options {
+  border: 1px solid #1f204180;
+}
+
+.dropdown__value {
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  padding-left: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 44px;
+}
+
+.dropdown__options {
+  display: none;
+  flex-direction: column;
+  row-gap: 7px;
+  padding: 7px 0;
+  border-bottom-left-radius: 3px;
+  border-bottom-right-radius: 3px;
+  border-top: none;
+}
+
+.dropdown__option {
+  padding: 0 7px 0 15px;
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 10px;
+}
+
+.dropdown__option-control {
+  background-color: transparent;
+  border: 1px solid #1f204180;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  color: #1f204180;
+  cursor: pointer;
+}
+
+.dropdown__option-text {
+  font-weight: 600;
+  text-transform: uppercase;
+  margin-left: 0;
+  margin-right: auto;
+}
+
+.dropdown__expand-button {
+  background-image: url(@/assets/arrow.svg);
+  display: flex;
+  align-items: center;
+  background-position: center;
+  width: 44px;
+  height: 44px;
+  border: 0;
+  background-color: transparent;
+  background-repeat: no-repeat;
+  cursor: pointer;
+  transform: rotateX(180deg);
+}
+
+.dropdown_open .dropdown__expand-button {
+  transform: none;
+}
+
+.dropdown_open .dropdown__options {
+  display: flex;
+}
+</style>
